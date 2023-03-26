c75d0a02a17ec01c6b2e9ead6299af0ea26801c9
sengokyu <sengokyu+gh@gmail.com>
2023-03-26 11:35:20 +0900

    Implement GET operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { catchError, map, Observable, of } from 'rxjs';
+
+interface Restaurant {
+  name: string;
+  description: string;
+}
+
+interface Entry<T> {
+  id: number;
+  attributes: T;
+}
+
+interface Response {
+  data: Entry<Restaurant>[];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'strapi-sample-for-document';
+export class AppComponent implements OnInit {
+  error: any | undefined;
+  restaurants$: Observable<Restaurant[]> | undefined;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const url = 'http://localhost:1337/api/restaurants';
+    const opts = { params: { populate: '*' } };
+
+    this.restaurants$ = this.http.get<Response>(url, opts).pipe(
+      catchError((error) => this.handleError(error)),
+      map((response) => response.data.map((x) => x.attributes))
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    this.error = error;
+    return of();
+  }
 }
